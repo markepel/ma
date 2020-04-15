95f803be5844e2b9c0dc67a1c1b66d7639dd2cab
Epel Mark <epelmark@gmail.com>
2020-04-15 16:28:06 +0300

    fx

--- rapid_capture_and_streaming.py
+++ rapid_capture_and_streaming.py
@@ -74,7 +74,10 @@ for i in range(retry_count):
         logging.info('connection write ends')
     except Exception as e:
         logging.error('Exception when retrying streaming{}'.format(e), exc_info=True)
-        logging.info(f'camera_manager.get_total_images_count {camera_manager.get_total_images_count()}')
+        try:
+            logging.info(f'camera_manager.get_total_images_count {camera_manager.get_total_images_count()}')
+        except:
+            pass
     finally:
         try:
             new_connection.close()

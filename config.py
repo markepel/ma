9f803f5f7f8474768d03b5cee776ce66c507304f
Epel Mark <epelmark@gmail.com>
2020-05-04 16:39:23 +0300

    1920-1080

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ generation_period=60
 tilda_port=8008
 camera_resolution=(640, 480)
 camera_framerate=3
-videocamera_resolution=(2592, 1944)
+videocamera_resolution=(1920, 1080)
 streaming_time=60
 retry_count=30
